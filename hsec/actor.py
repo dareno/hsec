701836c431c6849fe35e3a703df28290f9bf4027
dareno <dcreno@gmail.com>
2016-03-11 15:09:29 +0000

    now threads prevent blocking while waiting to receive

--- hsec/actor.py
+++ hsec/actor.py
@@ -7,9 +7,30 @@ Receive events, decide what to do. Based on zguide.
 import zmq
 import requests
 import configparser
+import queue
+from threading import Thread
+import time
+
+def recv_msg(q, subscriber):
+    """
+    Block until messages are received, put them in the queue
+    """
+
+    while True:
+        # get the envelope and message, put it on the shared queue
+        [address, contents] = subscriber.recv_multipart()
+        q.put([address,contents])
+        # unlock the queue for others to read from
+        #q.task_done()
+
 
 def main():
     """ main method """
+
+    # get key for ifttt maker recipe
+    config = configparser.ConfigParser()
+    config.read('actor.cfg')
+    key=config['maker.ifttt.com']['Key']
 
     # Prepare our context and publisher
     context    = zmq.Context()
@@ -18,21 +39,31 @@ def main():
     subscriber.setsockopt(zmq.SUBSCRIBE, b"State")
     subscriber.setsockopt(zmq.SUBSCRIBE, b"Events")
 
-    config = configparser.ConfigParser()
-    config.read('actor.cfg')
-    #key = "something"
-    key=config['maker.ifttt.com']['Key']
+    q = queue.Queue(maxsize=0)
+    worker = Thread(target=recv_msg, args=(q,subscriber,))
+    worker.setDaemon(True)
+    worker.start()
 
-    while True:
-        # Read envelope with address
-        [address, contents] = subscriber.recv_multipart()
-        #print("[%s] %s" % (address, contents))
-        post = "https://maker.ifttt.com/trigger/front_door_opened/with/key/" + key
-        print(post)
-        #print(requests.post(post))
+    try:
+        while True:
+            # Read envelope and address from queue
+            try:
+                [address, contents] = q.get(False)
+            except queue.Empty:
+                time.sleep(0.1)
+            else:
+                q.task_done()
+                print("[%s] %s" % (address, contents))
 
+            # trigger an event
+            #post = "https://maker.ifttt.com/trigger/front_door_opened/with/key/" + key
+            #print(post)
+            #print(requests.post(post))
 
-    # We never get here but clean up anyhow
+    except KeyboardInterrupt:
+        q.join(timeout=1)
+
+    # clean up zmq connection
     subscriber.close()
     context.term()
 
